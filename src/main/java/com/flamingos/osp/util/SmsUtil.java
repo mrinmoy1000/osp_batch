ffd324a2ea6b42184f7744a95815f796fe6a01d8
computer4 <computer4@104.239.213.7>
2016-01-21 20:00:31 +0530

    Commit after removing hardcoding on smsUtil class

--- src/main/java/com/flamingos/osp/util/SmsUtil.java
+++ src/main/java/com/flamingos/osp/util/SmsUtil.java
@@ -7,23 +7,31 @@ import java.net.URLEncoder;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
+@Configuration
+@PropertySource("classpath:osp.properties")
 public class SmsUtil {
+	@Value("${sms.userid}")
+	private String userId;
+	@Value("${sms.password}")
+	private String password;
 
-	public static String sendSms(SmsBean smsBean)
+	public String sendSms(SmsBean smsBean)
 	{
-		String responseMessage;
 		
+		
+		String responseMessage;
 		try {
-			String username = "ccgspl";
-			String password = "ccgspl";
-
+			
 			String requestUrl = "http://sms.ismilez.in/sendsms.jsp?user="
-					+ URLEncoder.encode(username, "UTF-8") + "&" + "password="
+					+ URLEncoder.encode(userId, "UTF-8") + "&" + "password="
 					+ URLEncoder.encode(password, "UTF-8") + "&mobiles="
 					+ URLEncoder.encode(smsBean.getRecipient(), "UTF-8") + "&sms="
 					+ URLEncoder.encode(smsBean.getMessage(), "UTF-8") + "&senderid=CCGSPL";
