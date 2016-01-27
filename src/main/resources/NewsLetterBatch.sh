1a4b6e42f6b21afab0e131438670dcc97ec3a5f8
system2 <sauvik@lamingostech.com>
2016-01-27 18:07:01 +0530

    NewsLetterBatch.sh added

---
+++ src/main/resources/NewsLetterBatch.sh
@@ -0,0 +1,3 @@
+echo "Executing Newsletter Batch..."
+java -jar /home/system2/workspace/osp_batch/target/OSP_BATCH_DIST/osp-batch.jar com.flamingos.tech.osp.batch.LaunchJobNewsLetter
+echo "Newsletter Batch Executed..."
