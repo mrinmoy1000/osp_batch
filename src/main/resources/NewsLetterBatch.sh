08133f9de333afedf0ffa88e0291e748dc2dfc51
system2 <sauvik@lamingostech.com>
2016-01-28 13:20:42 +0530

    Cron expression in NewsLetterBatch.sh

--- src/main/resources/NewsLetterBatch.sh
+++ src/main/resources/NewsLetterBatch.sh
@@ -3,3 +3,6 @@ echo "Executing Newsletter Batch..."
 $JAVA_HOME/bin/java -jar osp-batch.jar com.flamingos.tech.osp.batch.LaunchJobNewsLetter
 
 echo "Newsletter Batch Executed..."
+
+# Cron expression to exeute NewsLetterBatch.sh file everyday 1:00 A.M.
+#1 00 * * * sh /home/system2/workspace/osp_batch/target/OSP_BATCH_DIST/NewsLetterBatch.sh 
